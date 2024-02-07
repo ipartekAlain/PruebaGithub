0a73e445a5509574a5d18bbfc5bac57100052caa
Alain <107689010+ipartekAlain@users.noreply.github.com>
2024-02-07 09:34:49 +0100

    Modelo de datos:viaje
    crada la tabla de viajes

--- src/main/java/com/ipartek/model/Viaje.java
+++ src/main/java/com/ipartek/model/Viaje.java
@@ -1,5 +1,78 @@
 package com.ipartek.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Viaje {
 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="id")
+	private String nombre;
+	
+	
+	@Column(name="id")
+	private int precio;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+
+	public Viaje(int id, String nombre, int precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+	
+	public Viaje() {
+		super();
+		this.id = 0;
+		this.nombre = "";
+		this.precio = 0;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Viaje [id=" + id + ", nombre=" + nombre + ", precio=" + precio + "]";
+	}
+	
+	
+	
+	
+	
 }
