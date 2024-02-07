0e2c5ce868c88523eab6fd03efab6508bc801884
Alain <107689010+ipartekAlain@users.noreply.github.com>
2024-02-07 09:58:32 +0100

    Documentacion
    Documentación acabada

--- src/main/java/com/ipartek/model/Viaje.java
+++ src/main/java/com/ipartek/model/Viaje.java
@@ -1,18 +1,42 @@
 package com.ipartek.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+
+/**
+ * @author Alain
+ * @version 1.0
+ * 
+ * <p>Clase Viajes</p>
+ * <p>Clase DTO generada con Hybernate para represntar la tabla que creara en
+ * la BD</p>
+ * 
+ */
+@Entity
+@Table(name="viajes")
 public class Viaje {
 
 
+	/**
+	 * Atributo Id: admite valores numericos, pero al insertar un 
+	 * viaje nuevo en la BD se le asignara el valor automaticamente
+	 * 
+	 * al hacer una modificacion, deberemos añadirle el valor del id delç
+	 * objeto a modificar....
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
+	/**
+	 * Atributo Nombre: describimos lo que tiene el atributo nombre
+	 */
 	@Column(name="id")
 	private String nombre;
 	
